d9c2e7775eba50bb6bb80f44b4daa2c856fbd19a
Nusrat <nusratilla.88@gmail.com>
2024-05-16 13:54:13 +0500

    navBar-mob

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,34 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom'
 import Logo from '../images/vabuchiW.png'
 import { links } from '../data'
 import { FaBarsStaggered } from "react-icons/fa6";
+import { MdOutlineClose } from "react-icons/md";
 import './navbar.css'
 
 
 const Navbar = () => {
+  const [isNavShow, setIsNavShow] = useState(false);
+
   return (
     <nav>
       <div className="container nav-container">
         <Link to='/' className='logo'>
           <img src={Logo} alt="" />
         </Link>
-        <ul className="nav-links">
+        <ul className= {`nav-links ${isNavShow ? 'show-nav' : 'hide-nav'}`}>
           {
             links.map(({ name, path }, index) => {
               return (
-                <li>
-                  <NavLink to={path} className={({isActive}) => isActive ? 'active-nav' : ''}>{ name}</NavLink>
+                <li key={index}>
+                  <NavLink to={path} className={({isActive}) => isActive ? 'active-nav' : null}>{ name}</NavLink>
                 </li>
               )
             })
           }
         </ul>
-        <button className='nav-toggle-btn'>
-        <FaBarsStaggered />
+        <button className='nav-toggle-btn' onClick={() => setIsNavShow(!isNavShow)}>
+          {isNavShow ? <MdOutlineClose/> : <FaBarsStaggered/>}
           </button>
       </div>
 
