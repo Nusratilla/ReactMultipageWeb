b0b30e2d7a9bd46eb146111b56db17b9143f4062
Nusrat <nusratilla.88@gmail.com>
2024-05-26 00:35:58 +0500

    a

--- src/components/AbtInfo.jsx
+++ src/components/AbtInfo.jsx
@@ -2,6 +2,9 @@
 import { IoDiamondOutline } from "react-icons/io5";
 import SectionHead from "./SectionHead";
 import { abtInfo } from "../data";
+import Card from "../UI/Card";
+import { Link } from "react-router-dom";
+import { FaAngleRight } from "react-icons/fa";
 
 const AbtInfo = () => {
   return (
@@ -14,9 +17,16 @@ const AbtInfo = () => {
       <div className="abt-info-wrapper">
         {
           abtInfo.map(({ id, icon, title, info, path }) => {
-            
+            return (
+               <Card className="abt-info-card" key={id}>
+                 <span>{icon}</span>
+                 <h4>{title}</h4>
+                 <small>{info}</small>
+                 <Link to={path} className="btn sm">Ko'proq <FaAngleRight /></Link>
+               </Card>
+             )            
           })
-}
+        }
       </div>
     </section>
   )
